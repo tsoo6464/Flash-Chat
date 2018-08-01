8f5a97d79731cccde12090b85893bac0cfaffb23
tsoo6464 <tsoo6464@gmail.com>
2018-08-01 22:27:50 +0800

    send message

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -26,20 +26,17 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
-        
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         //TODO: Set yourself as the delegate of the text field here:
-
-        
-        
+        messageTextfield.delegate = self
         //TODO: Set the tapGesture here:
-        
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
 
         //TODO: Register your MessageCell.xib file here:
-
-        
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTableView()
     }
 
     ///////////////////////////////////////////
@@ -49,19 +46,25 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
-    
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        let msgArray = ["First Message", "Second Message", "Third Message"]
+        cell.messageBody.text = msgArray[indexPath.row]
+        return cell
+    }
     //TODO: Declare numberOfRowsInSection here:
-    
-    
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     //TODO: Declare tableViewTapped here:
-    
-    
-    
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     //TODO: Declare configureTableView here:
-    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
@@ -72,12 +75,19 @@ class ChatViewController: UIViewController {
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
 
     
     ///////////////////////////////////////////
@@ -90,13 +100,27 @@ class ChatViewController: UIViewController {
     
     
     @IBAction func sendPressed(_ sender: AnyObject) {
-        
-        
+        guard let message = messageTextfield.text, messageTextfield.text != "" else { return }
+        messageTextfield.endEditing(true)
         //TODO: Send the message to Firebase and save it in our database
-        
-        
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        // 建立database連結
+        let messageDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["SenderId": Auth.auth().currentUser?.email,
+                                 "MessageBody": message]
+        // 自動產生ID並儲存
+        messageDB.childByAutoId().setValue(messageDictionary) { (error, reference) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message saved successfully!")
+                self.messageTextfield.text = ""
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+            }
+        }
     }
-    
     //TODO: Create the retrieveMessages method here:
     
     
